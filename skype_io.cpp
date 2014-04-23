f18656f6e16e8f5b1ceda16dbb129635295b405f
trodevel <trodevel@gmail.com>
2014-04-24 00:33:58 +0200

    sync with svn

--- skype_io.cpp
+++ skype_io.cpp
@@ -19,12 +19,13 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 */
 
-// $Id: skype_io.cpp 427 2014-04-22 17:01:05Z serge $
+// $Id: skype_io.cpp 438 2014-04-23 16:45:36Z serge $
 
 #include "skype_io.h"       // self
 
 #include "../utils/wrap_mutex.h"    // SCOPE_LOCK
 #include "../utils/dummy_logger.h"  // dummy_log
+#include "command_gen.h"            // CommandGen
 
 #define MODULENAME      "SkypeIo"
 
@@ -73,7 +74,7 @@ bool SkypeIo::is_inited() const
     return sw_.is_inited();
 }
 
-bool SkypeIo::send( const std::string & s, std::string & response )
+bool SkypeIo::send_raw( const std::string & s, std::string & response )
 {
     SCOPE_LOCK( mutex_ );
 
@@ -112,5 +113,112 @@ void SkypeIo::main_thread()
 {
     sw_.main_thread();
 }
+
+// Skype interface
+bool SkypeIo::call( const std::string & s )
+{
+    std::string cmd = CommandGen::call( s );
+
+    SCOPE_LOCK( mutex_ );
+
+    return sw_.send( cmd, response_ );
+}
+bool SkypeIo::get_call_property( uint32 id, const std::string & s )
+{
+    std::string cmd = CommandGen::get_call_property( id, s );
+
+    SCOPE_LOCK( mutex_ );
+
+    return sw_.send( cmd, response_ );
+}
+bool SkypeIo::set_call_status( uint32 id, call_status_e s )
+{
+    std::string cmd = CommandGen::set_call_status( id, s );
+
+    SCOPE_LOCK( mutex_ );
+
+    return sw_.send( cmd, response_ );
+}
+bool SkypeIo::alter_call_hangup( uint32 id )
+{
+    std::string cmd = CommandGen::alter_call_hangup( id );
+
+    SCOPE_LOCK( mutex_ );
+
+    return sw_.send( cmd, response_ );
+}
+bool SkypeIo::alter_call_set_input_soundcard( uint32 id )
+{
+    std::string cmd = CommandGen::alter_call_set_input_soundcard( id );
+
+    SCOPE_LOCK( mutex_ );
+
+    return sw_.send( cmd, response_ );
+}
+bool SkypeIo::alter_call_set_input_port( uint32 id, uint32 p )
+{
+    std::string cmd = CommandGen::alter_call_set_input_port( id, p );
+
+    SCOPE_LOCK( mutex_ );
+
+    return sw_.send( cmd, response_ );
+}
+bool SkypeIo::alter_call_set_input_file( uint32 id, const std::string & s )
+{
+    std::string cmd = CommandGen::alter_call_set_input_file( id, s );
+
+    SCOPE_LOCK( mutex_ );
+
+    return sw_.send( cmd, response_ );
+}
+bool SkypeIo::alter_call_set_output_soundcard( uint32 id )
+{
+    std::string cmd = CommandGen::alter_call_set_output_soundcard( id );
+
+    SCOPE_LOCK( mutex_ );
+
+    return sw_.send( cmd, response_ );
+}
+bool SkypeIo::alter_call_set_output_port( uint32 id, uint32 p )
+{
+    std::string cmd = CommandGen::alter_call_set_output_port( id, p );
+
+    SCOPE_LOCK( mutex_ );
+
+    return sw_.send( cmd, response_ );
+}
+bool SkypeIo::alter_call_set_output_file( uint32 id, const std::string & s )
+{
+    std::string cmd = CommandGen::alter_call_set_output_file( id, s );
+
+    SCOPE_LOCK( mutex_ );
+
+    return sw_.send( cmd, response_ );
+}
+bool SkypeIo::alter_call_set_capture_mic_port( uint32 id, uint32 p )
+{
+    std::string cmd = CommandGen::alter_call_set_capture_mic_port( id, p );
+
+    SCOPE_LOCK( mutex_ );
+
+    return sw_.send( cmd, response_ );
+}
+bool SkypeIo::alter_call_set_capture_mic_file( uint32 id, const std::string & s )
+{
+    std::string cmd = CommandGen::alter_call_set_capture_mic_file( id, s );
+
+    SCOPE_LOCK( mutex_ );
+
+    return sw_.send( cmd, response_ );
+}
+
+// should be called after each Skype command
+std::string SkypeIo::get_response() const
+{
+    SCOPE_LOCK( mutex_ );
+
+    return response_;
+}
+
 
 NAMESPACE_SKYPE_WRAP_END
--- skype_io.h
+++ skype_io.h
@@ -19,7 +19,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 */
 
-// $Id: skype_io.h 427 2014-04-22 17:01:05Z serge $
+// $Id: skype_io.h 438 2014-04-23 16:45:36Z serge $
 
 #ifndef SKYPE_IO_H
 #define SKYPE_IO_H
@@ -48,12 +48,30 @@ public:
 
     bool register_handler( ISkypeCallback * eh );
 
-    bool send( const std::string & s, std::string & response );
+    bool send_raw( const std::string & s, std::string & response );
+
+    // Skype interface
+    bool call( const std::string & s );
+    bool get_call_property( uint32 id, const std::string & s );
+    bool set_call_status( uint32 id, call_status_e s );
+    bool alter_call_hangup( uint32 id );
+    bool alter_call_set_input_soundcard( uint32 id );
+    bool alter_call_set_input_port( uint32 id, uint32 p );
+    bool alter_call_set_input_file( uint32 id, const std::string & s );
+    bool alter_call_set_output_soundcard( uint32 id );
+    bool alter_call_set_output_port( uint32 id, uint32 p );
+    bool alter_call_set_output_file( uint32 id, const std::string & s );
+    bool alter_call_set_capture_mic_port( uint32 id, uint32 p );
+    bool alter_call_set_capture_mic_file( uint32 id, const std::string & s );
+
+    // should be called after each Skype command
+    std::string get_response() const;
 
     void control_thread();
     void main_thread();
 
     std::string get_error_msg() const;
+
 
 private:
 
@@ -64,6 +82,8 @@ private:
     SkypeWrap               sw_;
 
     EventGen                event_gen_;
+
+    std::string             response_;
 };
 
 NAMESPACE_SKYPE_WRAP_END
